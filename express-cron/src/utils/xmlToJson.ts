56db2795b9668b0f4958cbc6dee7a8a296949fba
Amir Arab <amir4rab@gmail.com>
2022-07-23 15:56:16 +0430

    added express-cron utils

---
+++ express-cron/src/utils/xmlToJson.ts
@@ -0,0 +1,32 @@
+const { XMLParser, XMLBuilder, XMLValidator} = require('fast-xml-parser');
+
+interface itemSchema {
+  id: number,
+  reg1: string
+  dis1: string
+  reg2: string
+  dis2: string
+  reg3: string
+  dis3: string
+  mag: string
+  dep: string
+  long: string
+  lat: string
+  date: string
+}
+
+const xmlToJson = ( XMLdata: string ): itemSchema[] | null => {
+  try {
+    const parser = new XMLParser();
+  
+    const jObj = parser.parse( XMLdata );
+    const returnedArray = jObj.items.item.slice(1) as itemSchema[];
+  
+    return returnedArray;
+  } catch (err) {
+    console.error(`Error in "submitToDatabase": `, err);
+    return null
+  }
+};
+
+export default xmlToJson;
