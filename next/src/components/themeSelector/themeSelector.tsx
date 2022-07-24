cfa10ada2449835c0bbb6d8a9f0edc3db0fe1ad5
Amir Arab <amir4rab@gmail.com>
2022-07-24 10:07:47 +0430

    added ThemeSelector

---
+++ next/src/components/themeSelector/themeSelector.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+// mantine
+import { ActionIcon } from "@mantine/core"
+
+// colorscheme selector
+import { createStyles, useMantineColorScheme } from '@mantine/styles'
+
+// icons
+import { IoSunny, IoMoon } from 'react-icons/io5'
+import useTranslation from 'next-translate/useTranslation';
+
+const useStyles = createStyles((t) => ({
+  buttonWrapper: {
+    display: 'flex',
+    // justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: t.spacing.md
+  },
+  text: {
+    marginLeft: t.dir === 'rtl' ? t.spacing.md : 0,
+    marginRight: t.dir === 'ltr' ? t.spacing.md : 0,
+    color: t.white
+  }
+}));
+
+const ThemeSelector = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const { t } = useTranslation('common')
+  const { classes } = useStyles();
+
+
+  return (
+    <div className={ classes.buttonWrapper }>
+      <p className={ classes.text }>
+      { colorScheme === 'dark' ? t('light-mode') : t('dark-mode') }
+      </p>
+      <ActionIcon onClick={ () => toggleColorScheme() } aria-label={ colorScheme === 'dark' ? t('light-mode') : t('dark-mode') }>
+        { colorScheme === 'dark' ? <IoSunny /> : <IoMoon /> }
+      </ActionIcon>
+    </div>
+  )
+}
+
+export default ThemeSelector;
