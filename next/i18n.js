0c5601ebaa587b9818262577d8f6340947755de7
Amir Arab <amir4rab@gmail.com>
2022-07-26 16:11:29 +0430

    added translations

--- next/i18n.js
+++ next/i18n.js
@@ -1,9 +1,10 @@
 module.exports = {
   locales: ["en", "de", "fa"],
-  defaultLocale: "fa",
+  defaultLocale: "en",
   loadLocaleFrom: async (lang, ns) => 
     await require(`../i18n/${lang}/${ns}.json`),
   pages: {
-    "*": [ "common", "earthquake", "states" ]
+    "*": [ "common", "earthquake", "states" ],
+    "/states/[id]": [ "state" ]
   }
 }
