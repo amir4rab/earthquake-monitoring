3574fca964e99950f16d93163dee1951ad012290
Amir Arab <amir4rab@gmail.com>
2022-07-28 10:47:51 +0430

    added shared-data

---
+++ shared-data/types/states-geo-location.d.ts
@@ -0,0 +1,8 @@
+export declare interface StateGeoLocationInterface {
+  lat: number,
+  long: number
+}
+
+export declare interface StatesGeoLocationInterface {
+  [ v: string ]: StateGeoLocationInterface
+}
