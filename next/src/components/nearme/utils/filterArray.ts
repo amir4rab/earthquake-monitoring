96469e087ad872c9c1c14cdb73741495bfab38e1
Amir Arab <amir4rab@gmail.com>
2022-07-30 10:57:47 +0430

    added nearme utils

---
+++ next/src/components/nearme/utils/filterArray.ts
@@ -0,0 +1,14 @@
+// types
+import type { ExtendedEarthquake } from '../nearme';
+
+/** filters array with the maximum range */
+const filterArray = ( arr: ExtendedEarthquake[], distances: { min: number, max: number }, maximumRange: number ) => 
+  arr
+    .filter(({ distance }) => distance <= distances.min + ( distances.max - distances.min ) * maximumRange / 100 )
+    .sort((a, b) => {
+      if ( a.date < b.date ) return 1;
+      if ( a.date > b.date ) return -1;
+      return 0;
+    });
+
+export default filterArray;
