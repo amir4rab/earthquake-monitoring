c2074e55c8c02577fd303a00b320de29ec80eaf6
Amir Arab <amir4rab@gmail.com>
2022-07-30 10:56:26 +0430

    added utils to prisma

--- next/src/prisma/index.ts
+++ next/src/prisma/index.ts
@@ -1,3 +1,3 @@
 export * from './connect';
-
 export * from './utils/getStateEvents';
+export * from './utils/getLatestEvents';
---
+++ next/src/prisma/utils/getLatestEvents.ts
@@ -0,0 +1,30 @@
+import { prisma } from '@/prisma';
+
+// types
+import type { Earthquake } from '@prisma/client';
+
+type GetLatestEvents = (pageSize?: number, page?: number) => Promise<Earthquake[]>;
+
+export const getLatestEvents: GetLatestEvents = async (pageSize= 25, page= 0) => {
+  try {
+    const data = await prisma.earthquake.findMany({
+      include: {
+        city: {
+          select: {
+            name: true,
+            nameFa: true
+          }
+        }
+      },
+      orderBy: {
+        date: 'asc'
+      },
+      take: pageSize
+    });
+
+    return data
+  } catch (err) {
+    console.error(err);
+    return []
+  }
+};
