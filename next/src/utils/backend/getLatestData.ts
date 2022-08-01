4369ca81b024ef8314db5d622e5659c4e04a665d
Amir Arab <amir4rab@gmail.com>
2022-08-01 11:51:14 +0430

    added caching backend utils

---
+++ next/src/utils/backend/getLatestData.ts
@@ -0,0 +1,42 @@
+import type { ExtendedEarthquakeArray } from '@/types/extendedEarthquake';
+
+import { getLatestDataFromPostgres } from '@/prisma';
+import { getCachedLatestData, setLatestEarthquakeDataToCache, delCachedLatestData } from '@/redis';
+
+
+type GetLatestData = ( skipCache?: boolean ) => Promise<ExtendedEarthquakeArray>;
+
+const defaultReturn: ExtendedEarthquakeArray = [];
+
+/** Gets the latest data, either from cache or database */
+const getLatestData: GetLatestData = async (skipCache= false) => {
+  try {
+    if ( !skipCache ) {
+      const cachedStateData = await getCachedLatestData();
+    
+      if ( cachedStateData !== null ) return cachedStateData as ExtendedEarthquakeArray;
+    } else {
+      await delCachedLatestData()
+    }
+  
+    const latestEarthquakesArr = await getLatestDataFromPostgres( 25 );
+  
+    if ( latestEarthquakesArr.length === 0 ) return defaultReturn;
+  
+    
+    const latestSortedData = latestEarthquakesArr.sort((a, b) => {
+      if ( a.date > b.date ) return -1;
+      if ( b.date > a.date ) return 1;
+      return 0
+    })
+    
+    await setLatestEarthquakeDataToCache(latestSortedData as ExtendedEarthquakeArray);
+
+    return latestSortedData as ExtendedEarthquakeArray;
+  } catch(err) {
+    console.error(err);
+    return defaultReturn;
+  }
+};
+
+export default getLatestData;
---
+++ next/src/utils/backend/getStateData.ts
@@ -0,0 +1,60 @@
+import { GetStatesDataFromPostgres } from '@/prisma';
+import { getCachedSatesEarthquakeDataById, setEarthquakeDataToCache, delCachedStateDataById } from '@/redis';
+import { ExtendedEarthquakeArray } from '@/types/extendedEarthquake';
+
+export interface GetStateDataProps {
+  stateId: string, 
+  page: number, 
+  skipCache?: boolean
+};
+
+export interface GetStateDataReturn {
+  latestEarthquakesArr: ExtendedEarthquakeArray;
+  totalPages: number;
+};
+
+const defaultReturn: GetStateDataReturn = {
+  latestEarthquakesArr: [],
+  totalPages: 0
+}
+
+const pageSize = 25;
+
+/** Gets the latest data, either from cache or database */
+const getStateData: (v: GetStateDataProps) => Promise<GetStateDataReturn> = async ({ stateId, skipCache= false, page }) => {
+  try {
+    if ( !skipCache ) {
+      const cachedStateData = await getCachedSatesEarthquakeDataById(stateId);
+    
+      if ( cachedStateData !== null ) return ({
+        latestEarthquakesArr: cachedStateData[page] as ExtendedEarthquakeArray,
+        totalPages: cachedStateData.length - 1
+      });
+    } else {
+      await delCachedStateDataById(stateId);
+    }
+    
+    const { latestEarthquakesArr } = await GetStatesDataFromPostgres({ stateId, pageSize: 25 * 5 });
+  
+    if ( latestEarthquakesArr.length === 0 ) return defaultReturn;
+  
+    const slicedStateData: ExtendedEarthquakeArray[] = [];
+    const totalPages = Math.ceil(latestEarthquakesArr.length / pageSize);
+  
+    for ( let i = 0; i < totalPages; i++ ) {
+      slicedStateData.push(latestEarthquakesArr.slice(i * pageSize, (i+1)*pageSize) as ExtendedEarthquakeArray);
+    };
+  
+    await setEarthquakeDataToCache(slicedStateData, stateId);
+  
+    return ({
+      latestEarthquakesArr: slicedStateData[page] as ExtendedEarthquakeArray,
+      totalPages
+    });
+  } catch(err) {
+    console.error(err);
+    return defaultReturn;
+  }
+};
+
+export default getStateData;
