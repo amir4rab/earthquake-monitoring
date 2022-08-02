af854d463bb964c809a12d96d41cac631d80dc70
Amir Arab <amir4rab@gmail.com>
2022-08-02 11:08:32 +0430

    added home component

---
+++ next/src/components/home/home.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+// mantine components
+import { Title, Box, createStyles } from '@mantine/core'
+
+// next-translate
+import useTranslation from 'next-translate/useTranslation'
+
+// components
+import EarthquakeDisplay from '../earthquakeDisplay';
+
+// types
+import type { ExtendedEarthquakeArray } from '@/types/extendedEarthquake';
+
+const useStyles = createStyles((theme) => ({
+  main: {
+    padding: '10vh 0'
+  },
+}));
+
+export interface HomeComponentProps {
+  latestEarthquakesArr: ExtendedEarthquakeArray
+}
+
+const HomeComponent = ({ latestEarthquakesArr }: HomeComponentProps ) => {
+  const { classes } = useStyles();
+  const { t } = useTranslation('common')
+
+  return (
+    <Box className={ classes.main }>
+      <Title order={ 1 }>
+        { t('home') }
+      </Title>
+      <EarthquakeDisplay
+        title={ t('latest') }
+        latestEarthquakesArr={ latestEarthquakesArr }
+      />
+    </Box>
+  )
+}
+
+export default HomeComponent
