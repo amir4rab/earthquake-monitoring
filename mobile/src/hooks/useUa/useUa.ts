d2a74439707f2f08e5545c94b70e61b4e9a60820
Amir Arab <amir4rab@gmail.com>
2022-08-03 10:56:24 +0430

    added useUa hook

---
+++ mobile/src/hooks/useUa/useUa.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+import Ua from 'ua-parser-js';
+
+const useUa = () => {
+  const [ us ] = useState(new Ua().getResult());
+
+  return us;
+}
+
+export default useUa;
