dd24ef102767274f323d74b8a645b1a588f9003d
Amir Arab <amir4rab@gmail.com>
2022-08-03 10:56:11 +0430

    converted next hooks for mobile usage

---
+++ mobile/src/hooks/useGeoLocation/useGeoLocation.ts
@@ -0,0 +1,44 @@
+// react
+import { useCallback, useEffect, useState } from 'react';
+
+// mantine
+import { useLocalStorage } from '@mantine/hooks';
+
+const useGeoLocation = () => {
+  const [ geolocationPermission, setGeolocationPermission ] = useLocalStorage({ 
+    key: 'geolocation-permission', 
+    defaultValue: false 
+  });
+  const [ geolocationData, setGeolocationData ] = useState< null | { lat: number, long: number } >(null);
+  const [ failed, setFailed ] = useState< boolean >( import.meta.env.IS_NATIVE_BUNDLE === '1' ? true : false );
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  const getGeoLocation = useCallback(() => {
+    setIsLoading(true);
+
+    navigator.geolocation.getCurrentPosition(
+      (v) => {
+        setGeolocationData({ lat: v.coords.latitude, long: v.coords.longitude });
+        setIsLoading(false);
+      },
+      (err) => { console.error(err); setFailed(true) },
+      {
+        enableHighAccuracy: true
+      }
+    )
+  }, []);
+
+  useEffect(() => {
+    if ( typeof window !== 'undefined' && geolocationPermission ) getGeoLocation();
+  }, [ geolocationPermission, getGeoLocation ]);
+
+  return ({
+    geolocationPermission,
+    setGeolocationPermission,
+    geolocationData,
+    isLoading,
+    failed
+  })
+};
+
+export default useGeoLocation;
---
+++ mobile/src/hooks/useLatestData/useLatestData.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+import useSWR from 'swr';
+import fetcher from '@/swr-fetcher';
+
+// types
+import { ExtendedEarthquakeArray } from '@/types/extendedEarthquake';
+
+const useLatestData = () => {
+  const [ data, setData ] = useState([] as ExtendedEarthquakeArray)
+
+  const { data: response, isValidating } = useSWR( 
+    import.meta.env.VITE_NEXT_API_ROUTE + `/latest`,
+    ( ...props ) => fetcher<{ data: ExtendedEarthquakeArray, err: string | null }>( ...props )
+  );
+
+  useEffect(() => {
+    if ( !isValidating && typeof response?.data !== 'undefined' ) {
+      setData(response.data);
+    }
+  }, [ isValidating, response ]);
+
+  return ({
+    isValidating,
+    data,
+  })
+};
+
+export default useLatestData;
