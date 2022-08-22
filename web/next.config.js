c4838ac18fd45a524f0b5375124992dfffae98fc
amir4rab <60965542+amir4rab@users.noreply.github.com>
2022-08-22 06:03:07 +0000

    added prettier and configuration to eslint

--- web/next.config.js
+++ web/next.config.js
@@ -1,51 +1,53 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require('next-compose-plugins');
+
 const nextTranslate = require('next-translate');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true'
 });
 
-module.exports = withPlugins(
-  [
-    nextTranslate,
-    withBundleAnalyzer
-  ],
-  {
-    reactStrictMode: true,
-    swcMinify: true,
-    async redirects() {
-      return [
-        {
-          source: '/:locale*/marker-icon.png',
-          destination: '/assets/leaflet/marker-icon.svg',
-          permanent: false,
-          locale: false
-        },
-        {
-          source: '/:locale*/marker-icon-2x.png',
-          destination: '/assets/leaflet/marker-icon.svg',
-          permanent: false,
-          locale: false
-        },
-        {
-          source: '/:locale*/marker-shadow.png',
-          destination: '/assets/leaflet/shadow.png',
-          permanent: false,
-          locale: false
-        },
-        {
-          source: '/:locale*/layers.png',
-          destination: '/assets/leaflet/layers.svg',
-          permanent: false,
-          locale: false
-        },
-        {
-          source: '/:locale*/layers-2x.png',
-          destination: '/assets/leaflet/layers.svg',
-          permanent: false,
-          locale: false
-        }
-      ]
-    }
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    dirs: ['pages', 'src']
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:locale*/marker-icon.png',
+        destination: '/assets/leaflet/marker-icon.svg',
+        permanent: false,
+        locale: false
+      },
+      {
+        source: '/:locale*/marker-icon-2x.png',
+        destination: '/assets/leaflet/marker-icon.svg',
+        permanent: false,
+        locale: false
+      },
+      {
+        source: '/:locale*/marker-shadow.png',
+        destination: '/assets/leaflet/shadow.png',
+        permanent: false,
+        locale: false
+      },
+      {
+        source: '/:locale*/layers.png',
+        destination: '/assets/leaflet/layers.svg',
+        permanent: false,
+        locale: false
+      },
+      {
+        source: '/:locale*/layers-2x.png',
+        destination: '/assets/leaflet/layers.svg',
+        permanent: false,
+        locale: false
+      }
+    ];
   }
-);
+};
+
+module.exports = (_phase, { _ }) => {
+  const plugins = [nextTranslate, withBundleAnalyzer];
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig });
+};
